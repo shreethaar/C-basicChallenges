7231cf74673f532e9979c0e7013a0f8d1ddc6f53
shreethaar <shreethaara380@gmail.com>
2025-03-03 16:43:26 +0800

    [U26Bv5AhlFT6Y]

---
+++ effective-c/chap2/enums.c
@@ -0,0 +1,6 @@
+// enum allow to define a type that assign names to integer values 
+
+enum day {sun,mon,tue,wed,thu,fri,sat};
+enum cardinal_points {north=0,east=90,south=180,west=270};
+enum months { jan = 1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec};
+
