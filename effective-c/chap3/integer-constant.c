bea4c3760fdaf7e4e7b2211c340a032b0c4f975a
shreethaar <shreethaara380@gmail.com>
2025-03-05 18:16:54 +0800

    [DdL2BQpWUkNVg]

---
+++ effective-c/chap3/integer-constant.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    //decimal constants
+    unsigned int ui=71;
+    int si;
+    si=-12;
+
+    //octal constants
+    int agent=007;
+    int permissions=0777;
+
+    //hexadeciaml constants
+    int burger=0xDEADBEEF;
+
+    //appending suffix to constant for specifying its type
+    unsigned int ui = 71U;
+    signed long int sli = 9223372036854775807L;
+    unsigned long long int ui = 18446744073709551615ULL;
+    
+}
