33ce34b23e2a6e95a9a7f5e9f0dfac5e270ce72b
shreethaar <shreethaara380@gmail.com>
2025-03-05 18:17:04 +0800

    [UbL13jyMHBKyw]

---
+++ effective-c/chap3/unsigned-int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+
+int main() {
+    printf("Unsigned char: %u\n",UCHAR_MAX);
+    printf("Unsighed short: %u\n",USHRT_MAX);
+    printf("Unsigned int: %u\n",UINT_MAX);
+    printf("Unsigned long: %lu\n",ULONG_MAX);
+    printf("Unsigned long long: %llu\n",ULLONG_MAX);
+    return 0;
+}
+
