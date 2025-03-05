acabb86a18c94122a32efadfd390689b2fdd5e24
shreethaar <shreethaara380@gmail.com>
2025-03-05 18:16:47 +0800

    [TBEFATKpQ0yVf]

---
+++ effective-c/chap3/declaring-int.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    unsigned int ui; //unsigned is required
+    unsigned u;      // int can be omitted
+    unsigned long long ull2;    // int can be omitted
+    unsigned char uc;           // unsigne is required
+
+
+    int i;      // signed can be omitted
+    long long int sll; //signed can be omitted
+    long long sll2; // signed and int can be omitted
+    signed char sc;
+                
+}
+
+// Declaring integers
+// unless explicitly declared as unsigne, integer types are assumed to be signed
+// declaring signed integer types, can omit the signed keyword - except for signed char 
