485eb6e4d6f5a2d333c7c2d173dc796ba92ac27a
shreethaar <shreethaara380@gmail.com>
2025-03-10 23:19:51 +0800

    [gK4uFTdneLL9A]

---
+++ gfg-50/q6.c
@@ -0,0 +1,17 @@
+// write a program to convert the binary number into a decimal number
+
+#include <stdio.h>
+
+int main() {
+    int N=11011;
+    int a =1;
+    int ans=0;
+    while(N!=0) {
+        ans=ans+(N%10)*a;
+        N=N/10;
+        a=a*2;
+    }
+    printf("%d",ans);
+    return 0;
+}
+
