3e278c352792766ae51c8c76ec19763bde10c1f9
shreethaar <shreethaara380@gmail.com>
2025-03-13 23:29:20 +0800

    [SGWjUQLqbv1TZ]

---
+++ gfg-50/q9.c
@@ -0,0 +1,67 @@
+// write a program to check if a number is an Armstrong number or not
+#include <stdio.h>
+
+int order(int n) {
+    int res=0;
+    while(n) {
+        res++;
+        n=n/10;
+    }
+    return res;
+}
+
+int power(int x, unsigned int y) {
+    if(y==0) {
+        return 1;
+    }
+    if(y%2==0) {
+        return power(x,y/2)*power(x,y/2);
+    }
+    return x*power(x,y/2)*power(x,y/2);
+}
+
+
+
+int  isArmstrong(int x) {
+    int n = order(x);
+    int temp=x,sum=0;
+    while(temp) {
+        int r=temp%10;
+        sum+=power(r,n);
+        temp=temp/10;
+    }
+    if(sum==x) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+int main() {
+    int x = 120;
+    if(isArmstrong(x)==1) {
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+    }
+    
+    x=1634;
+    if(isArmstrong(x)==1) {
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+    }
+
+    x=1634;
+    if(isArmstrong(x)==1) {
+        printf("True\n");
+    }
+    else {
+        printf("False\n");
+    }
+    return 0;
+}
+
